3e97361392f10c085e6b77c765d074414159a66b
Jamila <j.r.cba@hotmail.com>
2023-12-20 18:51:46 +0300

    solved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+// import myimage from "./pexels-photo-1666065.jpeg";
+import { Myediting } from "./mywork";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Myediting />
+      {/* <img src={myimage} /> */}
     </div>
   );
 }
---
+++ src/mywork.js
@@ -0,0 +1,9 @@
+export function Myediting() {
+  return (
+    <div justify-content="center">
+      <h1>Gifts Shop</h1>
+      <p>Choose your fauvorite gift</p>
+      {/* <img src={myimage} /> */}
+    </div>
+  );
+}
