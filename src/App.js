7e14ca2553950b8035fdd07ec18f7fcf3f460110
Jamila <j.r.cba@hotmail.com>
2023-12-20 20:19:29 +0300

    task2Solving

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@
 import "./App.css";
 import myimage from "./pexels-photo-1666065.jpeg";
 import { Myediting } from "./mywork";
+import { products } from "./products";
 
 function App() {
   return (
     <div className="container">
       <Myediting />
       <img src={myimage} alt="gifts" width="500px" />
+      {/* <h1>{products[0].name}</h1>
+      <h2>{products[0].price} KD</h2>
+      <img src={products[0].image} />
+      <h1>{products[1].name}</h1>
+      <h2>{products[1].price} KD</h2>
+      <img src={products[1].image} width="30%" /> */}
+      {products.map((item) => (
+        <>
+          <h1>{item.name}</h1>
+          <h2>{item.price}</h2>
+          <img src={item.image} width="30%" />
+        </>
+      ))}
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,20 @@
+import myimage from "./bookmark.jpeg";
+import image2 from "./ring.jpeg";
+
+export const products = [
+  {
+    name: "BookMark",
+    price: 3,
+    image: myimage,
+  },
+  {
+    name: "Ring",
+    price: 150,
+    image: image2,
+  },
+  {
+    name: "Ring",
+    price: 150,
+    image: image2,
+  },
+];
