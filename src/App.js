423367d7538f3135462e8148c2494f5ff8301f6a
Jamila <j.r.cba@hotmail.com>
2023-12-20 19:22:51 +0300

    task1-solved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
-// import myimage from "./pexels-photo-1666065.jpeg";
+import myimage from "./pexels-photo-1666065.jpeg";
 import { Myediting } from "./mywork";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Myediting />
-      {/* <img src={myimage} /> */}
+      <img src={myimage} alt="gifts" width="500px" />
     </div>
   );
 }
